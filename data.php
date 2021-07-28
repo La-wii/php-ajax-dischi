cccf761750b1009973324a034c0efa61182bfc6e
La-wii <sentilavale@gmail.com>
2021-07-28 14:36:51 +0200

    fine esercizio

--- data.php
+++ data.php
@@ -2,20 +2,20 @@
 
     $dischi = [
         [
-            'title' => 'Gemitaiz',
-            'author' => 'Nonostante tutto',
+            'title' => 'Nonostante tutto',
+            'author' => 'Gemitaiz',
             'year' => 2018,
             'poster' => 'https://lh3.googleusercontent.com/proxy/YR6xtctlzhZ0_-BrV7teCEn-vOLJooK9XYkcD8sdxoMA37CczIIxc6txvv91nyWyY0d7mVL0xncdftjy5p_K0NJ4jwUX0Nj-UPdv2Lb4TpZVt8u4C6jxKrD1r5f6EdOLosew9Dlmxqer2ALWvv6tacxnftI4J1uV4DIDz6uK74M'
         ],
         [
-            'title' => 'Mecna',
-            'author' => 'Mentre nessuno guarda',
+            'title' => 'Mentre nessuno guarda',
+            'author' => 'Mecna',
             'year' => 2021,
             'poster' => 'https://images.genius.com/d4081b60920ec4c3f95fea42997f4716.1000x1000x1.jpg'
         ],
         [
-            'title' => 'Caneda',
-            'author' => 'La dolce vita',
+            'title' => 'La dolce vita',
+            'author' => 'Caneda',
             'year' => 2020,
             'poster' => 'https://www.hano.it/wp-content/uploads/Caneda-Cover.jpg'
         ],
@@ -44,7 +44,7 @@
             'poster' => 'https://a6p8a2b3.stackpathcdn.com/62DM4Fro61GTGf-xQaKlZ8uzGsY=/300x300/smart/rockol-img/img/foto/upload/re-mida-piano-solo-lazza-cover-ts1570149599.jpeg'
         ],
         [
-            'title' => 'D\io',
+            'title' => 'Dio',
             'author' => 'Dargen',
             'year' => 2019,
             'poster' => 'https://www.nuovecanzoni.com/wp-content/uploads/2014/12/DiO-cd-cover-Dargen-DAmico.jpg'
--- index-php.php
+++ index-php.php
@@ -17,9 +17,23 @@
     <link rel="stylesheet" href="css/style.css">
     <!-- collegamento font-awesome -->
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" integrity="sha512-iBBXm8fW90+nuLcSKlbmrPcLa0OT92xO1BIsZ+ywDWZCvqsWgccV3gFoRBv0z+8dLJgyAHIhR35VZc2oM/gI1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <!-- collegamento google font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Style+Script&display=swap" rel="stylesheet">
     <title>Document</title>
 </head>
 <body>
+
+    <header>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-12 text-center">
+                    <h1>Musicheria</h1>
+                </div>
+            </div>
+        </div>
+    </header>
 
     <main>
         <div class="container-fluid">
@@ -30,19 +44,25 @@
                     <div class="col-3">
 
                         <div class="card mb-5" style="width: 14rem;">
+
                             <img class="card-img-top" src="<?php echo $dischi[$i]['poster'];?>" alt="Card image cap">
+
                             <div class="card-body text-light">
+                                
+                                <h3 class="card-title">
+                                    <?php echo $dischi[$i]['author'];?>
+                                </h3>
+
                                 <h5 class="card-title">
                                     <?php echo $dischi[$i]['title'];?>
                                 </h5>
-                                <h5 class="card-title">
-                                    <?php echo $dischi[$i]['author'];?>
-                                </h5>
+
                                 <p class="card-text">
-                                <?php echo $dischi[$i]['year'];?> 
+                                    <?php echo $dischi[$i]['year'];?> 
                                 </p>
                                 
                             </div>
+
                         </div>
                         
                     </div>
