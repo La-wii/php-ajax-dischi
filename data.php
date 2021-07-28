a88940d75c2af7187ba576e6bdf2284645c28221
La-wii <sentilavale@gmail.com>
2021-07-28 14:01:19 +0200

    add new data

--- data.php
+++ data.php
@@ -2,28 +2,52 @@
 
     $dischi = [
         [
-            'title' => 'New Jersey',
-            'author' => 'Bon Jovi',
-            'year' => 1988,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+            'title' => 'Gemitaiz',
+            'author' => 'Nonostante tutto',
+            'year' => 2018,
+            'poster' => 'https://lh3.googleusercontent.com/proxy/YR6xtctlzhZ0_-BrV7teCEn-vOLJooK9XYkcD8sdxoMA37CczIIxc6txvv91nyWyY0d7mVL0xncdftjy5p_K0NJ4jwUX0Nj-UPdv2Lb4TpZVt8u4C6jxKrD1r5f6EdOLosew9Dlmxqer2ALWvv6tacxnftI4J1uV4DIDz6uK74M'
         ],
         [
-            'title' => 'New Jersey',
-            'author' => 'Bon Jovi',
-            'year' => 1988,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+            'title' => 'Mecna',
+            'author' => 'Mentre nessuno guarda',
+            'year' => 2021,
+            'poster' => 'https://images.genius.com/d4081b60920ec4c3f95fea42997f4716.1000x1000x1.jpg'
         ],
         [
-            'title' => 'New Jersey',
-            'author' => 'Bon Jovi',
-            'year' => 1988,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+            'title' => 'Caneda',
+            'author' => 'La dolce vita',
+            'year' => 2020,
+            'poster' => 'https://www.hano.it/wp-content/uploads/Caneda-Cover.jpg'
         ],
         [
-            'title' => 'New Jersey',
-            'author' => 'Bon Jovi',
+            'title' => 'Ye',
+            'author' => 'Kanye West',
+            'year' => 2018,
+            'poster' => 'https://www.melamorsicata.it/wp-content/uploads/2018/06/Kanye_West_Ye-530x529.jpg'
+        ],
+        [
+            'title' => 'Qualcosa cambierà',
+            'author' => 'Ghemon',
+            'year' => 2015,
+            'poster' => 'http://images.genius.com/4e226a1162753891edfaa7556f731490.300x300x1.jpg'
+        ],
+        [
+            'title' => 'Potere',
+            'author' => 'Luchè',
             'year' => 1988,
-            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/51sBr4IWDwL.jpg'
+            'poster' => 'https://images-na.ssl-images-amazon.com/images/I/B1FgUFb67DS._AC_SX679_.jpg'
+        ],
+        [
+            'title' => 'Re Mida',
+            'author' => 'Lazza',
+            'year' => 2019,
+            'poster' => 'https://a6p8a2b3.stackpathcdn.com/62DM4Fro61GTGf-xQaKlZ8uzGsY=/300x300/smart/rockol-img/img/foto/upload/re-mida-piano-solo-lazza-cover-ts1570149599.jpeg'
+        ],
+        [
+            'title' => 'D\io',
+            'author' => 'Dargen',
+            'year' => 2019,
+            'poster' => 'https://www.nuovecanzoni.com/wp-content/uploads/2014/12/DiO-cd-cover-Dargen-DAmico.jpg'
         ]
     ];
 
